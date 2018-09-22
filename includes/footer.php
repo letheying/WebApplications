c9f2b3d13b8c51dd7db1933a41aea14e48098bc1
qike ying <yingqike@gmail.com>
2018-09-22 15:33:49 -0400

    /includes

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<footer>
+	<p>&copy; 2017 Company, Inc.</p>
+</footer>
+
+</div><!--/.container-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="node_modules/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="node_modules/vendor/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="node_modules/ie10-viewport-bug-workaround.js"></script>
+
+  
+  </body>
+</html>
